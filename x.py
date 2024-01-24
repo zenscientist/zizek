17d393da9846cf523f6f99f59453dfa8744caf24
zenscientist <40191205+zenscientist@users.noreply.github.com>
2024-01-23 21:58:20 -0600

    Add files via upload

---
+++ x.py
@@ -0,0 +1,10 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+print(50 // 6)
+print(int(3.9))
+def x():
+    name = 'heath' #local variable
+    age =30
+    print('My name is ' + name + ', and my age is ' + str(age))
+x()
